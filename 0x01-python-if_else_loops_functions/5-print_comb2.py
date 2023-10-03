48a3af0b912e1844a3f2184e0978053b758ce3d8
DawitSishu <dawitalemayhu55@gmail.com>
2023-10-03 08:04:38 -0400

    edit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(99)
-        break
-    print("{}{}".format(0, i), end=", ")
+separator = ", "
+for i in range(0, 100):
+    if i < 10:
+        print("{}{}".format(0, i), end=separator)
+    else:
+        if i == 99:
+            separator = "\n"
+        print(i, end=separator)
