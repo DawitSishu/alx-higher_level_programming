3a71981557f2aea8697fcc2728ac85f99bc4fad1
DawitSishu <dawitalemayhu55@gmail.com>
2023-10-03 04:36:31 -0400

    prin in upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) > 97 and ord(str[i]) < 123:
+            print(f"{chr(ord(str[i]) - 32)}", end="")
+        else:
+            print(f"{str[i]}", end="")
