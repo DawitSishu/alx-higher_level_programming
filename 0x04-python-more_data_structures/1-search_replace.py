a31d7a8b884e4fd2ae9009074a018151ab06d05d
DawitSishu <dawitalemayhu55@gmail.com>
2023-10-11 06:38:01 -0400

    fixes

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if len(my_list) > 0:
-        new_matrix = my_list.copy()
-        for i in range(len(new_matrix)):
-            if new_matrix[i] == search:
-                new_matrix[i] = replace
-        return new_matrix
+    return [replace if num == search else num for num in my_list]
