b5ccb6a552fc97c7c5fd1a4b38ab284efdb1e3c9
DawitSishu <dawitalemayhu55@gmail.com>
2023-12-12 07:33:58 -0500

    edited

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,13 +1,2 @@
 -- lists all available databases
-SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM
-    information_schema.COLUMNS
-WHERE
-    TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
