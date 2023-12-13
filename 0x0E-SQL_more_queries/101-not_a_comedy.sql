51bd69f30694f828aa5cd01c9a7b6dc9ee75ccab
DawitSishu <dawitalemayhu55@gmail.com>
2023-12-13 04:24:29 -0500

    rating

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -11,7 +11,6 @@ FROM `tv_shows` AS t
                 FROM `tv_shows` AS t
 	             INNER JOIN `tv_show_genres` AS s
 		     ON s.`show_id` = t.`id`
-
 		     INNER JOIN `tv_genres` AS g
 		     ON g.`id` = s.`genre_id`
 		     WHERE g.`name` = "Comedy")
