7e0291da592bd5d08b2209191dcb4724c3f6aa4e
DawitSishu <dawitalemayhu55@gmail.com>
2023-12-13 03:52:03 -0500

    city

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- lists all available databases
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
