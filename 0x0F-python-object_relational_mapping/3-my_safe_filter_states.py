e48cb3dd1c0b63a2adbd50d8b5af9340a823a6cd
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-12 05:25:46 -0500

    safe search

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+safe search
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    pas = sys.argv[2]
+    nme = sys.argv[3]
+
+    search = sys.argv[4]
+
+    # defalt port - 3306
+    db = MySQLdb.connect(user=usr, passwd=pas, db=nme)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id",
+                (searched_name, ))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
