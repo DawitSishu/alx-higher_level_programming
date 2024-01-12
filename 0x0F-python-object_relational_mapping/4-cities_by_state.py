0a4187f35552f1ebf2e5acf93804d1b756e6647d
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-12 05:29:00 -0500

    cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+all cities
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    pas = sys.argv[2]
+    nme = sys.argv[3]
+
+    # defalt port - 3306
+    db = MySQLdb.connect(user=usr, passwd=pas, db=nme)
+    cur = db.cursor()
+
+    cur.execute("SELECT c.id, c.name, s.name \
+                 FROM cities c INNER JOIN states s \
+                 ON c.state_id = s.id \
+                 ORDER BY c.id")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
