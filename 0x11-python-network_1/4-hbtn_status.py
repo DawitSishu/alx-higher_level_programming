16c9952ece320635d2fa3e3b1b95e6a51c3200b7
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-26 06:46:24 -0500

    statsu

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""fectch htbn site status"""
+if __name__ == "__main__":
+    import requests
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    response = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
