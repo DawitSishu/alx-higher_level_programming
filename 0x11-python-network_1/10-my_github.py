45da722041519758283670d831678e2609954218
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-26 06:52:43 -0500

    my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""sends giub cedentials and displays the github id"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    auth = (argv[1], argv[2])
+    request = requests.get("https://api.github.com/user", auth=auth)
+    print(request.json().get("id"))
