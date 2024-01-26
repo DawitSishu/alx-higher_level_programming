7ca327b0fd56ef8c985f78435ef730494cacbe76
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-26 06:42:08 -0500

    ss

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""script that sends a request with url"""
+
+from urllib import request
+from sys import argv
+
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as page:
+        print(page.getheader("X-Request-Id"))
